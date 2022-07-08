ec62fde0b3ee198955cb8c4725f5e0aa8dd0ae93
mametarou967 <mametarou967@gmail.com>
2022-07-08 15:14:43 +0900

    initial commit

---
+++ SampleApplicationDemo/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SampleApplicationDemo.Views;
+
+namespace SampleApplicationDemo
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var w = new MainWindow();
+
+            w.Show();
+        }
+    }
+}
